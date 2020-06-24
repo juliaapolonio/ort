1c608a981c9e416697f735935ab7d53cea5094a3
juliaapolonio <apoloniojulia@gmail.com>
2020-06-24 17:03:11 -0300

    feat(*): Upload and save image with Flask

--- app/__init__.py
+++ app/__init__.py
@@ -2,9 +2,11 @@ from flask import Flask
 
 
 app = Flask(__name__)
+UPLOAD_FOLDER = 'app/static/data'
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 app.secret_key = 's3cr3t'
 app.config["DEBUG"] = True
 
 
-from app.controllers import default
+from app.controllers import default, processing
 
--- app/controllers/default.py
+++ app/controllers/default.py
@@ -1,8 +1,31 @@
 from app import app
-from flask import render_template
-#from processing import crl, img_click, ratio, script
+from flask import render_template, request,  flash, redirect, url_for, send_from_directory
+from werkzeug.utils import secure_filename
+import os
+from app.controllers.processing import allowed_file
 
 
 @app.route("/", methods=["GET","POST"])
 def index():    
+    if request.method == 'POST':
+        # check if the post request has the file part
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        file = request.files['file']
+        # if user does not select file, browser also
+        # submit an empty part without filename
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            return redirect(url_for('uploaded_file',
+                                    filename=filename))
     return render_template('index.html')
+
+@app.route('/uploads/<filename>')
+def uploaded_file(filename):
+    return send_from_directory(app.config['UPLOAD_FOLDER'],filename)
+
